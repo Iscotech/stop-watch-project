d7fc31886aba3a3c99870f80cb383443271cc698
Isaac <iscotech594@gmal.com>
2024-09-18 12:38:42 +0100

    latest commit

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -2,10 +2,62 @@ import React, { useState, useEffect, useRef } from "react";
 
 const StopWatch = () => {
   const [isRunning, setIsRunning] = useState(false);
-  const [elapsed, setElapsed] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
   const intervalId = useRef(null);
+
   const startTimeRef = useRef(0);
-  return <div></div>;
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalId.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 10);
+    }
+    return () => {
+      clearInterval(intervalId.current);
+    };
+  }, [isRunning]);
+
+  const start = () => {
+    setIsRunning(true);
+    startTimeRef.current = Date.now();
+    console.log(startTimeRef.current);
+  };
+
+  const stop = () => {
+    setIsRunning(false);
+  };
+
+  const reset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  };
+
+  const formatTime = () => {
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    let seconds = Math.floor((elapsedTime / 1000) % 60);
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+    return `${minutes}: ${seconds}: ${milliseconds}`;
+  };
+
+  return (
+    <div className="stopwatch">
+      <div className="display">{formatTime()}</div>
+      <div className="controls">
+        <button onClick={start} className="startBtn">
+          start
+        </button>
+        <button onClick={stop} className="stopBtn">
+          Stop
+        </button>
+        <button onClick={reset} className="resetBtn">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default StopWatch;
